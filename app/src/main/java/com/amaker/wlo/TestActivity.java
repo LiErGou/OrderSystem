5ab54cd3326dd94d008a1945bf433bc460ad5d7b
LiErGou <lichangliangsky@126.com>
2017-05-13 21:44:05 +0800

    5.13

--- app/src/main/java/com/amaker/wlo/TestActivity.java
+++ app/src/main/java/com/amaker/wlo/TestActivity.java
@@ -1,170 +1,56 @@
 package com.amaker.wlo;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.TextView;
-import android.widget.ToggleButton;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 
+/**
+ * 时间拾取器界面
+ *
+ * @author wwj_748
+ *
+ */
+public class TestActivity extends Activity {
+	/** Called when the activity is first created. */
+	private EditText startDateTime;
+	private EditText endDateTime;
 
-import com.shizhefei.view.indicator.FragmentListPageAdapter;
-import com.shizhefei.view.indicator.IndicatorViewPager;
-import com.shizhefei.view.indicator.IndicatorViewPager.IndicatorFragmentPagerAdapter;
-import com.shizhefei.view.indicator.ScrollIndicatorView;
-import com.shizhefei.view.indicator.slidebar.DrawableBar;
-import com.shizhefei.view.indicator.slidebar.ScrollBar.Gravity;
-import com.shizhefei.view.indicator.transition.OnTransitionTextListener;
-
-public class TestActivity extends FragmentActivity {
-	private IndicatorViewPager indicatorViewPager;
-	private LayoutInflater inflate;
-	private String[] names = {"热门", "猜你喜欢", "喜好口味", "喜好食材", "历史点餐", "ICE CREAM SANDWICH", "JELLY BEAN", "KITKAT"};
-	private ScrollIndicatorView scrollIndicatorView;
-	private ToggleButton pinnedToggleButton;
-	private ToggleButton splitAutotoggleButton;
-	private int unSelectTextColor;
+	private String initStartDateTime = "2013年9月3日 14:44"; // 初始化开始时间
+	private String initEndDateTime = "2014年8月23日 17:44"; // 初始化结束时间
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_moretab);
-		splitAutotoggleButton = (ToggleButton) findViewById(R.id.toggleButton1);
-		pinnedToggleButton = (ToggleButton) findViewById(R.id.toggleButton2);
-		ViewPager viewPager = (ViewPager) findViewById(R.id.moretab_viewPager);
-		scrollIndicatorView = (ScrollIndicatorView) findViewById(R.id.moretab_indicator);
-		scrollIndicatorView.setBackgroundColor(Color.RED);
-		scrollIndicatorView.setScrollBar(new DrawableBar(this, R.drawable.round_border_white_selector, Gravity.CENTENT_BACKGROUND) {
-			@Override
-			public int getHeight(int tabHeight) {
-				return tabHeight - dipToPix(12);
-			}
+		setContentView(R.layout.test);
 
-			@Override
-			public int getWidth(int tabWidth) {
-				return tabWidth - dipToPix(12);
+		// 两个输入框
+		startDateTime = (EditText) findViewById(R.id.inputDate);
+		endDateTime = (EditText) findViewById(R.id.inputDate2);
+
+		Log.d("TestActivity","liolioliostartDateTime is:"+startDateTime);
+		startDateTime.setText(initStartDateTime);
+		endDateTime.setText(initEndDateTime);
+
+		startDateTime.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+
+				DateTimePickDialogUtil dateTimePicKDialog = new DateTimePickDialogUtil(
+						TestActivity.this, initEndDateTime);
+				dateTimePicKDialog.dateTimePicKDialog(startDateTime);
+
 			}
 		});
 
-		unSelectTextColor = Color.WHITE;
-		// 设置滚动监听
-		scrollIndicatorView.setOnTransitionListener(new OnTransitionTextListener().setColor(Color.RED, unSelectTextColor));
+		endDateTime.setOnClickListener(new OnClickListener() {
 
-		viewPager.setOffscreenPageLimit(2);
-		indicatorViewPager = new IndicatorViewPager(scrollIndicatorView, viewPager);
-		inflate = LayoutInflater.from(getApplicationContext());
-		indicatorViewPager.setAdapter(new MyAdapter(getSupportFragmentManager(),this));
-
-		// 默认true ，自动布局
-		splitAutotoggleButton.setChecked(scrollIndicatorView.isSplitAuto());
-		splitAutotoggleButton.setOnCheckedChangeListener(onCheckedChangeListener);
-		pinnedToggleButton.setOnCheckedChangeListener(onCheckedChangeListener);
-
-	}
-
-	private OnCheckedChangeListener onCheckedChangeListener = new OnCheckedChangeListener() {
-
-		@Override
-		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-			if (buttonView == splitAutotoggleButton) {
-				// 设置是否自动布局
-				scrollIndicatorView.setSplitAuto(isChecked);
-			} else if (buttonView == pinnedToggleButton) {
-				scrollIndicatorView.setPinnedTabView(isChecked);
-				// 设置固定tab的shadow，这里不设置的话会使用默认的shadow绘制
-				scrollIndicatorView.setPinnedShadow(R.drawable.tabshadow, dipToPix(4));
-				scrollIndicatorView.setPinnedTabBgColor(Color.RED);
+			public void onClick(View v) {
+				DateTimePickDialogUtil dateTimePicKDialog = new DateTimePickDialogUtil(
+						TestActivity.this, initEndDateTime);
+				dateTimePicKDialog.dateTimePicKDialog(endDateTime);
 			}
-		}
-	};
-
-	private int size = 5;
-
-	public void on3(View view) {
-		size = 3;
-		indicatorViewPager.getAdapter().notifyDataSetChanged();
+		});
 	}
-
-	public void on4(View view) {
-		size = 4;
-		indicatorViewPager.getAdapter().notifyDataSetChanged();
-	}
-
-	public void on5(View view) {
-		size = 5;
-		indicatorViewPager.getAdapter().notifyDataSetChanged();
-	}
-
-	public void on12(View view) {
-		size = 12;
-		indicatorViewPager.getAdapter().notifyDataSetChanged();
-	}
-
-	private class MyAdapter extends IndicatorFragmentPagerAdapter {
-		private Activity activity =null;
-		public MyAdapter(FragmentManager fragmentManager,Activity activity) {
-			super(fragmentManager);
-			this.activity=activity;
-		}
-
-		@Override
-		public int getCount() {
-			return size;
-		}
-
-		@Override
-		public View getViewForTab(int position, View convertView, ViewGroup container) {
-			if (convertView == null) {
-				convertView = inflate.inflate(R.layout.tab_top, container, false);
-			}
-			TextView textView = (TextView) convertView;
-			textView.setText(names[position % names.length]);
-			int padding = dipToPix(10);
-			textView.setPadding(padding, 0, padding, 0);
-			return convertView;
-		}
-
-		@Override
-		public Fragment getFragmentForPage(int position) {
-			MoreFragment fragment = new MoreFragment();
-			Log.i("..", "MoreFragment()新建对象：lioliolio" +fragment);
-			Bundle bundle = new Bundle();
-			bundle.putInt(MoreFragment.INTENT_INT_INDEX, position);
-			fragment.setArguments(bundle);
-			return fragment;
-		}
-
-		@Override
-		public int getItemPosition(Object object) {
-			//这是ViewPager适配器的特点,有两个值 POSITION_NONE，POSITION_UNCHANGED，默认就是POSITION_UNCHANGED,
-			// 表示数据没变化不用更新.notifyDataChange的时候重新调用getViewForPage
-			return PagerAdapter.POSITION_NONE;
-		}
-
-	}
-
-	;
-
-	/**
-	 * 根据dip值转化成px值
-	 *
-	 * @param dip
-	 * @return
-	 */
-	private int dipToPix(float dip) {
-		int size = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, getResources().getDisplayMetrics());
-		return size;
-	}
-
 }
